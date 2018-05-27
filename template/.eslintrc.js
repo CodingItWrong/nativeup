036d0878ea6edf77cd79e6a18f025898f9147e6d
Josh Justice <jjustice@bignerdranch.com>
2018-05-27 18:59:58 -0400

    Switch to my eslint config

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,16 +1,13 @@
 module.exports = {
-  extends: 'airbnb',
+  'extends': [
+    'codingitwrong',
+    'plugin:react/recommended',
+  ],
   parser: 'babel-eslint',
   env: {
     browser: true,
   },
   rules: {
-    'import/prefer-default-export': 'off',
-    'no-case-declarations': 'off',
-    // "no-use-before-define": 0,
-    'react/jsx-filename-extension': 'off',
-    "react/prefer-stateless-function": 0,
     'react/prop-types': 'off',
-    // "react/self-closing-comp": 0,
   }
 };
