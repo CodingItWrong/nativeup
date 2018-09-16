96586c04f4933597f0a3dd6ffdbf961233331d2a
Josh Justice <me@codingitwrong.com>
2018-09-16 13:51:31 -0400

    Use react-native-mock

--- template/tests/setup.js
+++ template/tests/setup.js
@@ -1,5 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import '@jonny/react-native-mock/mock';
 import chai from 'chai';
 import sinonChai from 'sinon-chai';
 
