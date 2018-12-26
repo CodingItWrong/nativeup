3da429518048a98e0009856d0e31c9264d37027d
Josh Justice <me@codingitwrong.com>
2018-12-26 09:53:55 -0600

    Store login token

--- template/api.js
+++ template/api.js
@@ -1,14 +1,44 @@
 import axios from 'axios';
+import Reactotron from 'reactotron-react-native';
+import Expo from 'expo';
+import { AsyncStorage } from 'react-native';
+
+const tokenStorageKey = /* ENTER A KEY HERE */;
+const baseURL = 'http://localhost:3000'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
   headers: {
     'Content-Type': 'application/vnd.api+json',
   },
 });
 
+export function loadToken() {
+  return AsyncStorage.getItem(tokenStorageKey)
+    .then(token => {
+      setAuthHeader(token);
+      return token;
+    });
+}
+
 export function setToken(token) {
-  api.defaults.headers['Authorization'] = `Bearer ${token}`;
+  setAuthHeader(token);
+
+  return AsyncStorage.setItem(tokenStorageKey, token);
+}
+
+export function clearToken() {
+  setAuthHeader(null);
+
+  return AsyncStorage.removeItem(tokenStorageKey);
+}
+
+function setAuthHeader(token) {
+  if (token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`;
+  } else {
+    delete api.defaults.headers['Authorization'];
+  }
 }
 
 export default api;
