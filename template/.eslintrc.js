744d5f84624b8fdca08a4a2070f04838c1ac8435
Josh Justice <me@codingitwrong.com>
2021-06-11 06:45:16 -0400

    Add import rules

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+    'no-duplicate-imports': 'error',
+    'sort-imports': [
+      'warn',
+      {ignoreDeclarationSort: true, ignoreMemberSort: false},
+    ], // alphabetize named imports - https://eslint.org/docs/rules/sort-imports
+  },
 };
