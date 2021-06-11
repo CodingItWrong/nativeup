d7c4702fe2a27c4caf0b6fb8786341bd52325cf2
Josh Justice <me@codingitwrong.com>
2021-06-11 08:14:22 -0400

    Fix eslint import

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['import'],
   rules: {
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     'no-duplicate-imports': 'error',
