11161bdf41c4ad7dd591ee2b163850bec6384257
Josh Justice <me@codingitwrong.com>
2021-06-12 07:08:49 -0400

    Quote linting

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   rules: {
     'import/order': ['warn', {alphabetize: {order: 'asc'}}], // group and then alphabetize lines - https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
     'no-duplicate-imports': 'error',
+    quotes: ['error', 'single', {avoidEscape: true}], // single quote unless using interpolation
     'sort-imports': [
       'warn',
       {ignoreDeclarationSort: true, ignoreMemberSort: false},
