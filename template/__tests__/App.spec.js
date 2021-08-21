1103e6307c0e6b34c8d60bb0e7bf0672f1a3875b
Josh Justice <me@codingitwrong.com>
2021-08-21 18:04:36 -0400

    Fix import order

--- template/__tests__/App.spec.js
+++ template/__tests__/App.spec.js
@@ -1,6 +1,6 @@
 import 'react-native';
-import React from 'react';
 import {render} from '@testing-library/react-native';
+import React from 'react';
 import App from '../App';
 
 describe('App', () => {
