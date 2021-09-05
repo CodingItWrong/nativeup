30ff746e3d3889f996d58d28eee36b557f688047
Josh Justice <me@codingitwrong.com>
2021-09-05 07:44:04 -0400

    Move test to same folder as component

--- template/App.spec.js
+++ template/App.spec.js
@@ -1,7 +1,7 @@
 import 'react-native';
 import {render} from '@testing-library/react-native';
 import React from 'react';
-import App from '../App';
+import App from './App';
 
 describe('App', () => {
   it('renders a hello message', () => {
