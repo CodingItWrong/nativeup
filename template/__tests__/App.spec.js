d0548783844db31b8c27152272aa4896fba7449a
Josh Justice <me@codingitwrong.com>
2021-09-05 07:24:18 -0400

    Fix incorrect test matcher

--- template/__tests__/App.spec.js
+++ template/__tests__/App.spec.js
@@ -6,6 +6,6 @@ import App from '../App';
 describe('App', () => {
   it('renders a hello message', () => {
     const {queryByText} = render(<App />);
-    expect(queryByText('Hello, React Native!')).toBeDefined();
+    expect(queryByText('Hello, React Native!')).not.toBeNull();
   });
 });
